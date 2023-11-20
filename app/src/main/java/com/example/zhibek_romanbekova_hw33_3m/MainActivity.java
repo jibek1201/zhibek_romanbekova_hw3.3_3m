f56a679a0bbd3b8495027e8530623d2afed83527
zhibek <romanbekovazibek@gmail.com>
2023-11-20 12:14:04 +0600

    Initial commit

---
+++ app/src/main/java/com/example/zhibek_romanbekova_hw33_3m/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.zhibek_romanbekova_hw33_3m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new MainFragment()).commit();
+    }
+}
